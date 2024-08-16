6da704af78215f2e8b37523cb757a885df93e77d
Valeria <valleriademelo@gmail.com>
2024-08-16 11:31:51 -0300

    feat: fetch data from API

--- httpreact/src/App.js
+++ httpreact/src/App.js
@@ -1,11 +1,39 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+
+const url = "http://localhost:3000/products";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  // 1 - Resgatando dados
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+
+        setProducts(data);
+      } catch (error) {
+        console.error("Erro ao buscar os produtos:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
-  </div>
+      <ul>
+        {products.map((product) => (
+          <li key={product.name}>{product.name} - R$ {product.prince}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
 export default App;
+
